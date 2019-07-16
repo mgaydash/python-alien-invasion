2f31ccbdca163a721a9c6b6c3c744ee92e16866d
mgaydash <mgaydash@dataforma.com>
2019-07-16 19:31:51 -0400

    Add comments. Fill the screen background

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,15 +3,24 @@ import pygame
 
 
 def run_game():
+    # Initialize the game and create our screen object
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
 
+    # This is the main game loop
     while True:
+
+        # This loop is for responding to user input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        screen.fill(bg_color)
+
+        # This (oddly named) method draws the screen
         pygame.display.flip()
 
+# Start the game
 run_game()
