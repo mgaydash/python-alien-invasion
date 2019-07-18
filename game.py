eacedf5c1d1921f3e866ad0a86e53b2692d56b96
mgaydash <mgaydash@dataforma.com>
2019-07-18 17:30:39 -0400

    Update ship to be more oop

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-from pygame.sprite import Group
-
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
@@ -20,17 +18,19 @@ class Game():
         self.pygame.display.set_caption(self.settings.title)
 
         # Create ship
-        self.ship = Ship(self.screen, self.settings.ship_speed_factor)
-
-        # Make bullet group
-        self.bullets = Group()
+        self.ship = Ship(self.pygame, self.settings)
 
         # Main game loop
         while True:
             self.check_events()
+
+            # Draw on screen
+            self.screen.fill(self.settings.bg_color)
+
             self.ship.update()
-            self.update_bullets()
-            self.update_screen()
+
+            # This (oddly named) method draws the screen
+            self.pygame.display.flip()
 
     def check_events(self):
         """Respond to keypresses and mouse events"""
@@ -38,48 +38,5 @@ class Game():
         for event in self.pygame.event.get():
             if event.type == self.pygame.QUIT:
                 self.sys.exit()
-            elif event.type == self.pygame.KEYDOWN:
-                if event.key == self.pygame.K_d:
-                    self.ship.moving_right = True
-                elif event.key == self.pygame.K_a:
-                    self.ship.moving_left = True
-                elif event.key == self.pygame.K_SPACE:
-                    self.fire_bullet()
-            elif event.type == self.pygame.KEYUP:
-                if event.key == self.pygame.K_d:
-                    self.ship.moving_right = False
-                elif event.key == self.pygame.K_a:
-                    self.ship.moving_left = False
-
-    def fire_bullet(self):
-        if len(self.bullets) < self.settings.bullets_allowed:
-            self.bullets.add(Bullet(
-                self.pygame,
-                self.screen,
-                self.ship,
-                self.settings.bullet_width,
-                self.settings.bullet_height,
-                self.settings.bullet_color,
-                self.settings.bullet_speed_factor
-            ))
-
-    def update_bullets(self):
-        """Update bullet position and delete offscreen bullets"""
-
-        self.bullets.update()
-        # Remove bullets that leave the top of the screen (y = 0)
-        for bullet in self.bullets.copy():
-            if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet)
-
-    def update_screen(self):
-        """Update images on the screen and flip to the new screen"""
-
-        # Draw on screen
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blit()
-        for bullet in self.bullets.sprites():
-            bullet.blit()
-
-        # This (oddly named) method draws the screen
-        self.pygame.display.flip()
+            else:
+                self.ship.handle_event(event)
--- ship.py
+++ ship.py
@@ -1,16 +1,23 @@
-import pygame
+from pygame.sprite import Group
+
+from bullet import Bullet
 
 class Ship():
     """The ship controlled by the user"""
 
-    def __init__(self, screen, speed):
-        self.screen = screen
-        self.speed = speed
+    def __init__(self, pygame, settings):
+        self.pygame = pygame
+        self.settings = settings
+        self.screen = self.pygame.display.get_surface()
+        self.speed = self.settings.ship_speed_factor
         self.moving_right = False
         self.moving_left = False
 
+        # Make bullet group
+        self.bullets = Group()
+
         # Load ship image and configure
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = self.pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
 
@@ -19,17 +26,57 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
         self.center = float(self.rect.centerx)
 
-    def blit(self):
-        """Draw the ship at it's current location"""
+    def handle_event(self, event):
+        if event.type == self.pygame.KEYDOWN:
+            if event.key == self.pygame.K_SPACE:
+                self.fire_bullet()
 
-        self.screen.blit(self.image, self.rect)
+            if event.key == self.pygame.K_d:
+                self.moving_right = True
+            elif event.key == self.pygame.K_a:
+                self.moving_left = True
+        elif event.type == self.pygame.KEYUP:
+            if event.key == self.pygame.K_d:
+                self.moving_right = False
+            elif event.key == self.pygame.K_a:
+                self.moving_left = False
+
+    
+    def fire_bullet(self):
+        """Fire a bullet"""
+        
+        if len(self.bullets) < self.settings.bullets_allowed:
+            self.bullets.add(Bullet(
+                self.pygame,
+                self.screen,
+                self,
+                self.settings.bullet_width,
+                self.settings.bullet_height,
+                self.settings.bullet_color,
+                self.settings.bullet_speed_factor
+            ))
 
     def update(self):
-        """Update the ship's position according to movement settings"""
+        """Update the ship's position and draw"""
 
+        # Update location according to movement settings
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.speed
         if self.moving_left and self.rect.left > self.screen_rect.left:
             self.center -= self.speed
-
         self.rect.centerx = self.center
+
+        # Update bullet locations
+        self.bullets.update()
+
+        # Remove bullets that leave the top of the screen (y = 0)
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+        # Draw bullets
+        for bullet in self.bullets.sprites():
+            bullet.blit()
+
+        # Draw
+        self.screen.blit(self.image, self.rect)
