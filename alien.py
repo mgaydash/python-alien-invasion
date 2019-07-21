ba6f1434003bbd1894d7a2eafb31e89b260aa793
mgaydash <mgaydash@dataforma.com>
2019-07-21 10:39:28 -0400

    Create a row of aliens

--- alien.py
+++ alien.py
@@ -1,7 +1,10 @@
-class Alien():
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
     """Enemy alien spaceship"""
 
-    def __init__(self, pygame):
+    def __init__(self, pygame, x_location):
+        super().__init__()
         self.pygame = pygame
         self.screen = self.pygame.display.get_surface()
 
@@ -11,11 +14,16 @@ class Alien():
         self.screen_rect = self.screen.get_rect()
 
         # Configure the alien's starting position
-        self.rect.x = self.rect.width
+        self.rect.x = x_location
         self.rect.y = self.rect.height
 
         # Store the x location as a float
         self.x = float(self.rect.x)
+
+    def get_width(self):
+        """Return the width of the alien sprite"""
+
+        return self.rect.width
 
     def update(self):
         """Update the alien's position and draw"""
--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from pygame.sprite import Group
+
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -22,7 +24,8 @@ class Game():
         self.pygame.display.set_caption(Game.title)
 
         self.ship = Ship(self.pygame)
-        self.alien = Alien(self.pygame)
+        self.aliens = Group()
+        self.create_fleet()
 
         # Main game loop
         while True:
@@ -32,7 +35,7 @@ class Game():
             self.screen.fill(Game.bg_color)
 
             self.ship.update()
-            self.alien.update()
+            self.aliens.update()
 
             # This (oddly named) method draws the screen
             self.pygame.display.flip()
@@ -45,3 +48,21 @@ class Game():
                 self.sys.exit()
             else:
                 self.ship.handle_event(event)
+
+    def create_fleet(self):
+        """Create a full fleet of aliens"""
+
+        # Create an example alien to use for measurment
+        alien = Alien(self.pygame, 0)
+        alien_width = alien.get_width()
+
+        # Want to allow padding of 2 * the sprite width
+        available_space_x = Game.screen_width - 2 * alien_width
+
+        # How many aliens can we fit? Allow padding between of 1 alien width
+        num_aliens_x = int(available_space_x / (2 * alien_width))
+
+        # Create rows of aliens
+        for alien_num in range(num_aliens_x):
+            alien_x = alien_width + 2 * alien_width * alien_num
+            self.aliens.add(Alien(self.pygame, alien_x))
